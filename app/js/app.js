df7e78753fe586892a9f3e66fae196a74fbbc0de
manumena <manuelmena1993@gmail.com>
2017-02-20 19:13:23 -0300

    Put languages in files

--- app/js/app.js
+++ app/js/app.js
@@ -15,27 +15,11 @@ app.config(['$routeProvider', function($routeProvider) {
 }]);
 
 app.config(['$translateProvider', function ($translateProvider) {
-  $translateProvider.translations('en', {
-  	'home': 'Emperador de las tinieblas',
-  	'kittens': 'Kittens',
-    'es': 'Spanish',
-    'en': 'English',
-    'welcome': 'Welcome!',
-    'description': 'This is a test page. If you come up with something to put in here do not doubt to contact me to emperadordelastinieblas_666@ashotashell.com.',
-    'kittens_title': 'And now, {{numberOfCats}} kittens'
-  });
-
-  $translateProvider.translations('es', {
-  	'home': 'Emperador de las tinieblas',
-  	'kittens': 'Gatitos',
-    'es': 'Español',
-    'en': 'Yanqui',
-    'welcome': 'Bienvenido!',
-    'description': 'Esta es una página de prueba. Si se te ocurre algo que poner acá no dudes en contactarme a emperadordelastinieblas_666@ashotashell.com.',
-    'kittens_title': 'A continuacion, {{numberOfCats}} gatitos'
-  });
-
-  $translateProvider.preferredLanguage('es');
+	$translateProvider.useStaticFilesLoader({
+		prefix: '/languages/locale-',
+		suffix: '.json'
+	});
+	$translateProvider.preferredLanguage('es');
 }]);
 
 app.controller('languageController', ['$translate', '$scope', function ($translate, $scope) {
